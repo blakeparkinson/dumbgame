e2c1831da633061b25bd31acc3c49779e89c708a
Blake Parkinson <wizardplow@gmail.com>
2013-03-17 01:36:46 -0700

    Create slayer.js

---
+++ games/slayer.js
@@ -0,0 +1,20 @@
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+
+while (slaying) {
+  if (youHit) {
+      console.log("You hit!");
+      totalDamage += damageThisRound;
+          if (totalDamage >= 4) {
+              console.log("You win!");
+              slaying = false;
+          } else {
+              youHit = Math.floor(Math.random() * 2);
+          }
+  } else {
+      console.log("You lose!");
+      slaying = false;
+  }
+}
