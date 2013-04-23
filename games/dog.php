b10214a8d04a009c83443fc7079b149f707594df
Blake Parkinson <blake@edmodo.com>
2013-04-23 00:49:17 -0700

    Update dog.php

--- games/dog.php
+++ games/dog.php
@@ -1,1 +1,30 @@
-y
+<!DOCTYPE html>
+<html>
+  <head>
+	  <title> Practice makes perfect! </title>
+      <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+      <p>
+        <?php
+       class Dog {
+            public $numLegs=4;
+            public $name;
+            public function __construct($name){
+                $this->name = $name;
+            }
+            public function bark(){
+                return "Woof!";
+            
+            }
+            public function greet() {
+                return "The name of the dog is" .$this->name. "!";
+            }
+        }
+        $dog1=new Dog("Barker");
+        $dog2=new Dog("Amigo");
+        echo $dog2 -> greet();
+        ?>
+      </p>
+    </body>
+</html>
