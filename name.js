2b2a77ab9f6ce3ac8de123ee0790589a4e2d97f9
Blake Parkinson <wizardplow@gmail.com>
2014-10-19 22:25:25 -0700

    Update name.js

--- name.js
+++ name.js
@@ -13,7 +13,6 @@ for (i=0;i<text.length;i++){
 	}
 	else {console.log("Your name wasn't found")};
 }
-
 var Todo = Backbone.Model.extend({
   // Default todo attribute values
   defaults: {
@@ -26,6 +25,12 @@ var todo1 = new Todo();
 console.log(todo1.get('title')); // empty string
 console.log(todo1.get('completed')); // false
 
+var todo2 = new Todo({
+  title: "Retrieved with model's get() method.",
+  completed: true
+});
+console.log(todo2.get('title')); // Retrieved with model's get() method.
+console.log(todo2.get('completed')); // true
 var todo2 = new Todo({
   title: "Retrieved with model's get() method.",
   completed: true
